2e72be52de0494449bd76c625981e54fa43085f3
Uriel Lugo <ulugo@na-at.com.mx>
2022-10-05 12:48:26 -0500

    [Add] ApplicationContext testing

--- src/test/java/com/uriellugo/udemyjunit/UdemyJUnitApplicationTests.java
+++ src/test/java/com/uriellugo/udemyjunit/UdemyJUnitApplicationTests.java
@@ -1,21 +1,64 @@
 package com.uriellugo.udemyjunit;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UdemyJUnitApplicationTests {
 
 	@Autowired
-	ApplicationContext context;
+	ConfigurableApplicationContext context;
+
+	@Autowired
+	Environment environment;
 
 	// https://www.jvt.me/posts/2021/06/25/spring-context-test/
 	@Test
 	void contextLoads() {
-		Assertions.assertNotNull(context);
+		assertNotNull(context);
 	}
 
+	@Test
+	void test_environment() {
+		String applicationName = environment.getProperty("spring.application.name");
+		assertNotNull(applicationName);
+		System.out.println("applicationName = " + applicationName);
+	}
+
+	@Test
+	void test_propertySources() {
+		System.out.println("Atributos del applicationContext");
+		System.out.println("run.getApplicationName() = " + context.getApplicationName());
+		System.out.println("run.getBeanDefinitionCount() = " + context.getBeanDefinitionCount());
+		System.out.println("run.getBeanDefinitionNames().length = " + context.getBeanDefinitionNames().length);
+
+		ConfigurableEnvironment environment = context.getEnvironment();
+		assertNotNull(environment, "\nEl environment es nulo");
+		System.out.println("environment.getSystemEnvironment() = " + environment.getSystemEnvironment());
+		System.out.println("environment.getSystemProperties() = " + environment.getSystemProperties());
+		System.out.println("environment.getProperty(\"properties.hello-world\") = " + environment.getProperty("properties.hello-world"));
+		System.out.println("environment.getActiveProfiles() = " + Arrays.toString(environment.getActiveProfiles()));
+
+		MutablePropertySources propertySources = environment.getPropertySources();
+		Iterator<PropertySource<?>> iterator = propertySources.stream().iterator();
+		System.out.println("\nConsiguiendo los propertySources del ApplicationContext");
+		System.out.println("--------------------------------------------------------\n");
+		for(int i=0; iterator.hasNext(); i++) {
+			PropertySource<?> next = iterator.next();
+			System.out.println("PropertySource" + (i) + ".getName() = " + next.getName());
+			System.out.println("PropertySource.getSource() = " + next.getSource());
+			System.out.println();
+		}
+	}
 }
